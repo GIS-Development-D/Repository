a867c99ec1c1c7a261418ac03bd83b6d2e71089b
Andrew <yu.1.xu@aalto.fi>
2024-04-19 23:33:08 +0300

    The whole workflow of the green infrastructure

--- umep_package.py
+++ umep_package.py
@@ -1,15 +1,14 @@
 import sys
 import os
-from qgis.core import (QgsApplication,
-                       QgsVectorLayer,
-                       QgsField,
-                       QgsVectorFileWriter)
+from qgis.core import QgsApplication
+from qgis.core import QgsVectorLayer
+from qgis.core import QgsField
+from qgis.core import QgsVectorFileWriter
 from qgis.PyQt.QtCore import QVariant
 
 # ----------------- Initialization of the QGIS related dependencies -----------------
 # Initiating a QGIS application
-home = 'D:/GIS-Development'
-QgsApplication.setPrefixPath(home, True)
+QgsApplication.setPrefixPath("D:/GIS-Development", True)
 qgs = QgsApplication([], False)
 qgs.initQgis()
 
@@ -29,8 +28,8 @@ umep_provider = ProcessingUMEPProvider()
 QgsApplication.processingRegistry().addProvider(umep_provider)
 
 
-# ----------------- UMEP Function Definition -----------------
-def CropDatasetWithMask(function_input_data, function_input_mask, function_input_crs, function_output_dir):
+# ----------------- UMEP Related Function Definition -----------------
+def cropDatasetWithMask(function_input_data, function_input_mask, function_input_crs, function_output_dir):
     return processing.run("gdal:cliprasterbymasklayer",
                           {'INPUT': function_input_data,
                            'MASK': function_input_mask,
@@ -42,7 +41,7 @@ def CropDatasetWithMask(function_input_data, function_input_mask, function_input
                            'DATA_TYPE': 0, 'EXTRA': '', 'OUTPUT': function_output_dir})
 
 
-def SkyViewFactor(function_input_cdsm, function_input_dsm, function_output_dir, function_output_file):
+def skyViewFactor(function_input_cdsm, function_input_dsm, function_output_dir, function_output_file):
     return processing.run("umep:Urban Geometry: Sky View Factor",
                           {'ANISO': True,
                            'INPUT_CDSM': function_input_cdsm,
@@ -53,7 +52,7 @@ def SkyViewFactor(function_input_cdsm, function_input_dsm, function_output_dir, 
                            'TRANS_VEG': 3})
 
 
-def WallHeightAspect(function_input_cdsm, function_output_height_file, function_output_aspect_file):
+def wallHeightAspect(function_input_cdsm, function_output_height_file, function_output_aspect_file):
     return processing.run("umep:Urban Geometry: Wall Height and Aspect",
                           {'INPUT': function_input_cdsm,
                            'INPUT_LIMIT': 3,
@@ -61,7 +60,75 @@ def WallHeightAspect(function_input_cdsm, function_output_height_file, function_
                            'OUTPUT_ASPECT': function_output_aspect_file})
 
 
-def TreePlanter(function_input_SOLWEIG, function_input_polygonlayer, function_output_dir):
+def run_solweig(input_dsm, input_cdsm, input_dem, input_meteo, svf_path, aniso_path, wallHeightRatioInputs, output_dir,
+                trans_veg=3, t_height=25, albedo_walls=0.2, albedo_ground=0.15, emis_walls=0.9, emis_ground=0.95,
+                abs_s=0.7, abs_l=0.95, posture=0, cyl=True, only_global=False, utc=0, poi_file=None,
+                poi_field='', age=35, activity=80, clo=0.9, weight=75, height=180, sex=0, sensor_height=10):
+    """
+    Run the SOLWEIG model for outdoor thermal comfort.
+
+    Parameters:
+    - input_dsm: Path to the Digital Surface Model (DSM) input file.
+    - input_cdsm: Path to the Canopy DSM input file.
+    - input_dem: Path to the Digital Elevation Model (DEM) input file.
+    - input_meteo: Path to the meteorological input file.
+    - svf_path: Dictionary containing the paths to SVF-related inputs.
+    - aniso_path: Dictionary containing the paths to aniso-related inputs.
+    - wallHeightRatioOutputs: Dictionary containing the paths to wall height ratio outputs.
+    - output_dir: Path to the output directory.
+    - Additional parameters correspond to SOLWEIG model options.
+
+    Returns:
+    - Output of SOLWEIG processing run.
+    """
+    solweig_params = {
+        'INPUT_DSM': input_dsm,
+        'INPUT_SVF': svf_path,
+        'INPUT_HEIGHT': wallHeightRatioInputs['OUTPUT_HEIGHT'],
+        'INPUT_ASPECT': wallHeightRatioInputs['OUTPUT_ASPECT'],
+        'INPUT_CDSM': input_cdsm,
+        'TRANS_VEG': trans_veg,
+        'INPUT_TDSM': None,
+        'INPUT_THEIGHT': t_height,
+        'INPUT_LC': None,
+        'USE_LC_BUILD': False,
+        'INPUT_DEM': input_dem,
+        'SAVE_BUILD': False,
+        'INPUT_ANISO': aniso_path,
+        'ALBEDO_WALLS': albedo_walls,
+        'ALBEDO_GROUND': albedo_ground,
+        'EMIS_WALLS': emis_walls,
+        'EMIS_GROUND': emis_ground,
+        'ABS_S': abs_s,
+        'ABS_L': abs_l,
+        'POSTURE': posture,
+        'CYL': cyl,
+        'INPUTMET': input_meteo,
+        'ONLYGLOBAL': only_global,
+        'UTC': utc,
+        'POI_FILE': poi_file,
+        'POI_FIELD': poi_field,
+        'AGE': age,
+        'ACTIVITY': activity,
+        'CLO': clo,
+        'WEIGHT': weight,
+        'HEIGHT': height,
+        'SEX': sex,
+        'SENSOR_HEIGHT': sensor_height,
+        'OUTPUT_TMRT': True,
+        'OUTPUT_KDOWN': False,
+        'OUTPUT_KUP': False,
+        'OUTPUT_LDOWN': False,
+        'OUTPUT_LUP': False,
+        'OUTPUT_SH': False,
+        'OUTPUT_TREEPLANTER': True,
+        'OUTPUT_DIR': output_dir
+    }
+
+    return processing.run("umep:Outdoor Thermal Comfort: SOLWEIG", solweig_params)
+
+
+def treePlanter(function_input_SOLWEIG, function_input_polygonlayer, function_output_dir):
     return processing.run("umep:Outdoor Thermal Comfort: TreePlanter",
                           {'SOLWEIG_DIR': function_input_SOLWEIG,
                            'INPUT_POLYGONLAYER': function_input_polygonlayer,
@@ -76,6 +143,51 @@ def TreePlanter(function_input_SOLWEIG, function_input_polygonlayer, function_ou
                            'OUTPUT_CDSM': True, 'OUTPUT_POINTFILE': True, 'OUTPUT_OCCURRENCE': False,
                            'OUTPUT_DIR': function_output_dir, 'ITERATIONS': 2000,
                            'INCLUDE_OUTSIDE': True, 'RANDOM_STARTING': False, 'GREEDY_ALGORITHM': False})
+
+
+def treeGenerator(function_input_pointlayer, function_input_dsm, function_input_dem, function_input_cdsm,
+                  function_output_cdsm, function_output_tdsm):
+    processing.run("umep:Spatial Data: Tree Generator",
+                   {
+                       'INPUT_POINTLAYER': function_input_pointlayer,
+                       'TREE_TYPE': 'ttype', 'TOT_HEIGHT': 'height', 'TRUNK_HEIGHT': 'trunk', 'DIA': 'diameter',
+                       'INPUT_BUILD': None,
+                       'INPUT_DSM': function_input_dsm,
+                       'INPUT_DEM': function_input_dem,
+                       'INPUT_CDSM': function_input_cdsm, 'INPUT_TDSM': None,
+                       'CDSM_GRID_OUT': function_output_cdsm, 'TDSM_GRID_OUT': function_output_tdsm})
+
+
+# This function is right now used to modify the attribute table of the generated trees
+def attributeTableMod(path, field_name, field_value, field_type=QVariant.Int):
+    layer = QgsVectorLayer(path)
+
+    if not layer.isValid():
+        print("Layer failed to load!")
+        return
+
+    print("Layer was loaded successfully!")
+
+    # Check if the field already exists
+    fields = layer.fields()
+    if field_name in fields.names():
+        print(f"Field '{field_name}' already exists. Skipping field addition and update.")
+        return
+
+    # Define the field type based on the input parameter
+    field = QgsField(field_name, field_type)
+
+    # Add the new field
+    layer.dataProvider().addAttributes([field])
+    layer.updateFields()
+
+    # Start editing to update field values
+    layer.startEditing()
+    for feature in layer.getFeatures():
+        feature.setAttribute(field_name, field_value)
+        layer.updateFeature(feature)
+    layer.commitChanges()
+    print(f"Field '{field_name}' added as type '{field.typeName()}' and all features updated with the value '{field_value}'.")
 
 
 # ----------------- SOLWEIG Function Example -----------------
@@ -95,62 +207,51 @@ def SOLWEIG_Example_Goteborg():
     output_dir = "Output_temp"
     output_SOLWEIG_dir = "Output_SOLWEIG"
 
-    crop_cdsm = CropDatasetWithMask(os.path.join(input_directory, input_cdsm),
+    crop_cdsm = cropDatasetWithMask(os.path.join(input_directory, input_cdsm),
                                     os.path.join(input_directory, input_mask),
                                     cdsm_epsg, os.path.join(output_dir,
                                                             "Crop_" + \
                                                             input_cdsm_filename + ".tif"))
 
     input_dsm_filename = input_dsm.split(".")[0]
-    crop_dsm = CropDatasetWithMask(os.path.join(input_directory, input_dsm), os.path.join(input_directory, input_mask),
+    crop_dsm = cropDatasetWithMask(os.path.join(input_directory, input_dsm), os.path.join(input_directory, input_mask),
                                    None, os.path.join(output_dir,
                                                       "Crop_" + \
                                                       input_dsm_filename + ".tif"))
 
     input_dem_filename = input_dem.split(".")[0]
-    crop_dem = CropDatasetWithMask(os.path.join(input_directory, input_dem), os.path.join(input_directory, input_mask),
+    crop_dem = cropDatasetWithMask(os.path.join(input_directory, input_dem), os.path.join(input_directory, input_mask),
                                    None, os.path.join(output_dir,
                                                       "Crop_" + \
                                                       input_dem_filename + ".tif"))
 
     input_landcover_filename = input_landcover.split(".")[0]
-    crop_landcover = CropDatasetWithMask(os.path.join(input_directory, input_landcover),
-                                         os.path.join(input_directory, input_mask), None, os.path.join(output_dir,
-                                                                                                       "Crop_" + \
-                                                                                                       input_landcover_filename + ".tif"))
+    crop_landcover = cropDatasetWithMask(os.path.join(input_directory, input_landcover),
+                                         os.path.join(input_directory, input_mask),
+                                         None, os.path.join(output_dir,
+                                                            "Crop_" + input_landcover_filename + ".tif"))
 
     # Calculates SVF from cropped data
-    svf_outputs = SkyViewFactor(crop_cdsm["OUTPUT"], crop_dsm["OUTPUT"], output_dir,
+    svf_outputs = skyViewFactor(crop_cdsm["OUTPUT"], crop_dsm["OUTPUT"], output_dir,
                                 os.path.join(output_dir, 'SkyViewFactor.tif'))
 
     # Calculates wall height and wall aspect from cropped data
-    wallHeightRatioOutputs = WallHeightAspect(crop_dsm["OUTPUT"], os.path.join(output_dir, 'wallHeight.tif'),
+    wallHeightRatioOutputs = wallHeightAspect(crop_dsm["OUTPUT"], os.path.join(output_dir, 'wallHeight.tif'),
                                               os.path.join(output_dir, 'WallAspect.tif'))
 
-    processing.run("umep:Outdoor Thermal Comfort: SOLWEIG",
-                   {'INPUT_DSM': crop_dsm["OUTPUT"],
-                    'INPUT_SVF': os.path.join(svf_outputs['OUTPUT_DIR'], 'svfs.zip'),
-                    'INPUT_HEIGHT': wallHeightRatioOutputs['OUTPUT_HEIGHT'],
-                    'INPUT_ASPECT': wallHeightRatioOutputs['OUTPUT_ASPECT'],
-                    'INPUT_CDSM': crop_cdsm["OUTPUT"],
-                    'TRANS_VEG': 3, 'INPUT_TDSM': None, 'INPUT_THEIGHT': 25,
-                    'INPUT_LC': crop_landcover["OUTPUT"],
-                    'USE_LC_BUILD': False,
-                    'INPUT_DEM': crop_dsm["OUTPUT"],
-                    'SAVE_BUILD': False,
-                    'INPUT_ANISO': os.path.join(svf_outputs['OUTPUT_DIR'], 'shadowmats.npz'),
-                    'ALBEDO_WALLS': 0.2, 'ALBEDO_GROUND': 0.15, 'EMIS_WALLS': 0.9, 'EMIS_GROUND': 0.95,
-                    'ABS_S': 0.7, 'ABS_L': 0.95, 'POSTURE': 0, 'CYL': True,
-                    'INPUTMET': os.path.join(input_directory, input_meteo),
-                    'ONLYGLOBAL': False, 'UTC': 0, 'POI_FILE': None, 'POI_FIELD': '', 'AGE': 35,
-                    'ACTIVITY': 80, 'CLO': 0.9, 'WEIGHT': 75, 'HEIGHT': 180, 'SEX': 0, 'SENSOR_HEIGHT': 10,
-                    'OUTPUT_TMRT': True, 'OUTPUT_KDOWN': False, 'OUTPUT_KUP': False, 'OUTPUT_LDOWN': False,
-                    'OUTPUT_LUP': False, 'OUTPUT_SH': False, 'OUTPUT_TREEPLANTER': False,
-                    'OUTPUT_DIR': output_SOLWEIG_dir})
+    run_solweig(input_dsm=crop_dsm["OUTPUT"],
+                input_cdsm=crop_cdsm["OUTPUT"],
+                input_dem=crop_dsm["OUTPUT"],
+                input_meteo=os.path.join(input_directory, input_meteo),
+                svf_path=os.path.join(svf_outputs['OUTPUT_DIR'], 'svfs.zip'),
+                aniso_path=os.path.join(svf_outputs['OUTPUT_DIR'], 'shadowmats.npz'),
+                wallHeightRatioInputs={'OUTPUT_HEIGHT': wallHeightRatioOutputs['OUTPUT_HEIGHT'],
+                                                 'OUTPUT_ASPECT': wallHeightRatioOutputs['OUTPUT_ASPECT']},
+                output_dir=output_SOLWEIG_dir)
 
 
 # ----------------- TreePlanter Function Example -----------------
-def TreePlanter_Example():
+def treePlanter_Example():
     # Input files definition
     input_directory = "Data/TreePlanterTestData"
     input_cdsm = os.path.join(input_directory, 'CDSM.tif')
@@ -164,36 +265,22 @@ def TreePlanter_Example():
     output_TreePlanter_position = os.path.join(output_TreePlanter_SOLWEIG_dir, 'Output_TreePlanter_Position')
 
     # Calculates SVF from cropped data
-    svf_outputs = SkyViewFactor(input_cdsm, input_dsm, output_dir,
+    svf_outputs = skyViewFactor(input_cdsm, input_dsm, output_dir,
                                 os.path.join(output_dir, 'SkyViewFactor.tif'))
 
     # Calculates wall height and wall aspect from cropped data
-    wallHeightRatioOutputs = WallHeightAspect(input_dsm, os.path.join(output_dir, 'wallHeight.tif'),
+    wallHeightRatioOutputs = wallHeightAspect(input_dsm, os.path.join(output_dir, 'wallHeight.tif'),
                                               os.path.join(output_dir, 'WallAspect.tif'))
 
-    processing.run("umep:Outdoor Thermal Comfort: SOLWEIG",
-                   {'INPUT_DSM': input_dsm,
-                    'INPUT_SVF': os.path.join(svf_outputs['OUTPUT_DIR'], 'svfs.zip'),
-                    'INPUT_HEIGHT': wallHeightRatioOutputs['OUTPUT_HEIGHT'],
-                    'INPUT_ASPECT': wallHeightRatioOutputs['OUTPUT_ASPECT'],
-                    'INPUT_CDSM': input_cdsm,
-                    'TRANS_VEG': 3, 'INPUT_TDSM': None, 'INPUT_THEIGHT': 25,
-                    'INPUT_LC': None,
-                    'USE_LC_BUILD': False,
-                    'INPUT_DEM': input_dem,
-                    'SAVE_BUILD': False,
-                    'INPUT_ANISO': os.path.join(svf_outputs['OUTPUT_DIR'], 'shadowmats.npz'),
-                    'ALBEDO_WALLS': 0.2, 'ALBEDO_GROUND': 0.15, 'EMIS_WALLS': 0.9,
-                    'EMIS_GROUND': 0.95,
-                    'ABS_S': 0.7, 'ABS_L': 0.95, 'POSTURE': 0, 'CYL': True,
-                    'INPUTMET': input_meteo,
-                    'ONLYGLOBAL': False, 'UTC': 0, 'POI_FILE': None, 'POI_FIELD': '', 'AGE': 35,
-                    'ACTIVITY': 80, 'CLO': 0.9, 'WEIGHT': 75, 'HEIGHT': 180, 'SEX': 0,
-                    'SENSOR_HEIGHT': 10,
-                    'OUTPUT_TMRT': True, 'OUTPUT_KDOWN': False, 'OUTPUT_KUP': False,
-                    'OUTPUT_LDOWN': False,
-                    'OUTPUT_LUP': False, 'OUTPUT_SH': False, 'OUTPUT_TREEPLANTER': True,
-                    'OUTPUT_DIR': output_TreePlanter_SOLWEIG_dir})
+    run_solweig(input_dsm=input_dsm,
+                input_cdsm=input_cdsm,
+                input_dem=input_dem,
+                input_meteo=input_meteo,
+                svf_path=os.path.join(svf_outputs['OUTPUT_DIR'], 'svfs.zip'),
+                aniso_path=os.path.join(svf_outputs['OUTPUT_DIR'], 'shadowmats.npz'),
+                wallHeightRatioInputs={'OUTPUT_HEIGHT': wallHeightRatioOutputs['OUTPUT_HEIGHT'],
+                                                 'OUTPUT_ASPECT': wallHeightRatioOutputs['OUTPUT_ASPECT']},
+                output_dir=output_TreePlanter_SOLWEIG_dir)
 
     processing.run("umep:Outdoor Thermal Comfort: TreePlanter",
                    {'SOLWEIG_DIR': output_TreePlanter_SOLWEIG_dir,
@@ -211,35 +298,25 @@ def TreePlanter_Example():
                     'INCLUDE_OUTSIDE': True, 'RANDOM_STARTING': False, 'GREEDY_ALGORITHM': False})
 
 
-def TreeGenerator(function_input_pointlayer, function_input_dsm, function_input_dem, function_input_cdsm,
-                  function_output_cdsm, function_output_tdsm):
-    processing.run("umep:Spatial Data: Tree Generator",
-                   {
-                       'INPUT_POINTLAYER': function_input_pointlayer,
-                       'TREE_TYPE': 'type', 'TOT_HEIGHT': 'height', 'TRUNK_HEIGHT': 'trunk', 'DIA': 'diameter',
-                       'INPUT_BUILD': None,
-                       'INPUT_DSM': function_input_dsm,
-                       'INPUT_DEM': function_input_dem,
-                       'INPUT_CDSM': function_input_cdsm, 'INPUT_TDSM': None,
-                       'CDSM_GRID_OUT': function_output_cdsm, 'TDSM_GRID_OUT': function_output_tdsm})
+# ----------------- Update the cdsm with New Trees Example -----------------
+def regenerate_cdsm():
+    input_directory = "Data/TreePlanterTestData"
+    input_cdsm = os.path.join(input_directory, 'CDSM.tif')
+    input_dsm = os.path.join(input_directory, 'DSM.tif')
+    input_dem = os.path.join(input_directory, 'DEM.tif')
 
+    output_TreePlanter_SOLWEIG_dir = "Output_TreePlanter"
+    output_TreePlanter_position = os.path.join(output_TreePlanter_SOLWEIG_dir, 'Output_TreePlanter_Position')
+    input_TreePlanter_layer = os.path.join(output_TreePlanter_position, 'treePlanterPoint.shp')
+    output_cdsm = "Output_TreeGenerator/cdsm.tif"
+    output_tdsm = "Output_TreeGenerator/tdsm.tif"
 
-# This function is right now used to modify the attribute table of the generated trees
-def AttributeTableMod(path, field_name, field_value):
-    layer = QgsVectorLayer(path, providerLib='ogr')
+    attributeTableMod(input_TreePlanter_layer, "ttype", 1)
+    treeGenerator(input_TreePlanter_layer, input_dsm, input_dem, input_cdsm, output_cdsm, output_tdsm)
 
-    if not layer.isValid():
-        print("Layer failed to load!")
-    else:
-        print("Layer was loaded successfully!")
-
-    layer.dataProvider().addAttributes([QgsField(field_name, QVariant.String)])
-    layer.updateFields()
-    layer.startEditing()
-    for feature in layer.getFeatures():
-        feature[field_name] = field_value
-        layer.updateFeature(feature)
-    layer.commitChanges()
 
 # SOLWEIG_Example_Goteborg()
-# TreePlanter_Example()
+# treePlanter_Example()
+
+# regenerate_cdsm()
+# treePlanter_Example()
